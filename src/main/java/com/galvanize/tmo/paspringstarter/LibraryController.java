9c5453c203de5c50de102fe71f6ce16188cdec9b
Onindita Afrin <37190114+onindita@users.noreply.github.com>
2021-10-07 13:52:16 -0600

    Update LibraryController.java

--- src/main/java/com/galvanize/tmo/paspringstarter/LibraryController.java
+++ src/main/java/com/galvanize/tmo/paspringstarter/LibraryController.java
@@ -1,13 +1,36 @@
 package com.galvanize.tmo.paspringstarter;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class LibraryController {
 
+    @Autowired
+    public LibraryService libraryService;
+
     @GetMapping("/health")
     public void health() {
 
+    }
+
+    @PostMapping("/api/books")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Book addBooks(@RequestBody Book newBook){
+        return libraryService.addBook(newBook);
+    }
+
+    @GetMapping("/api/books")
+    public List<Book> getBooks(){
+        return libraryService.getAllBooks();
+    }
+
+    @DeleteMapping("/api/books")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public String deleteBooks(){
+        return libraryService.deleteAllBooks();
     }
 }
