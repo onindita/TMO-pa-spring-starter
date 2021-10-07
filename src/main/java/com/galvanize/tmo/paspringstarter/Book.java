ebe89bb7cb0794b0497cebadaf1713a0e13ac15e
Onindita Afrin <37190114+onindita@users.noreply.github.com>
2021-10-07 13:52:44 -0600

    Add files via upload

---
+++ src/main/java/com/galvanize/tmo/paspringstarter/Book.java
@@ -0,0 +1,64 @@
+package com.galvanize.tmo.paspringstarter;
+
+public class Book implements Comparable<Book>{
+
+    private int id;
+    private String author;
+    private String title;
+    private int yearPublished;
+
+    public Book(int id, String author, String title, int yearPublished) {
+        this.id = id;
+        this.author = author;
+        this.title = title;
+        this.yearPublished = yearPublished;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getYearPublished() {
+        return yearPublished;
+    }
+
+    public void setYearPublished(int yearPublished) {
+        this.yearPublished = yearPublished;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", author='" + author + '\'' +
+                ", title='" + title + '\'' +
+                ", yearPublished=" + yearPublished +
+                '}';
+    }
+
+    @Override
+    public int compareTo(Book b) {
+
+        return getTitle().compareTo(b.getTitle());
+    }
+}
---
+++ src/main/java/com/galvanize/tmo/paspringstarter/LibraryService.java
@@ -0,0 +1,43 @@
+package com.galvanize.tmo.paspringstarter;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+@Service
+public class LibraryService {
+
+
+    List<Book> books = new ArrayList<>();
+
+    public List<Book> getAllBooks(){
+        Collections.sort(books);
+        return books;
+    }
+
+    public Book addBook(Book newBook){
+
+        int newId = getAllBooks().size() + 1;
+
+        newBook.setId(newId);
+
+        books.add(newBook);
+
+        return newBook;
+
+    }
+
+    public String deleteAllBooks(){
+
+        books.removeAll(books);
+
+        return "None";
+    }
+
+
+
+}
+
